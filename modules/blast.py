4f4b829afe2532a0c5a481ea1fbe43b87a2663ba
mars13 <marina.reixachs@gmail.com>
2016-03-07 13:24:41 +0100

    Blast exec

--- modules/blast.py
+++ modules/blast.py
@@ -1,9 +1,17 @@
 from Bio.Blast.Applications import NcbiblastpCommandline as blastp
+from Bio.Blast import NCBIXML
 
 def exec_blast(infile, config_file):
 	db, evalue = parse_blast_conf(config_file)
-	blastp_cline = blastp(query=infile, db=db, evalue = evalue, outfmt=5, out="")
+	blastp_cline = blastp(query=infile, db=db, evalue = evalue, outfmt=5, out="blast_out.xml")
+	stdout, stderr = blastp_cline()
 	
 def parse_blast_conf(config_file):
+	op_config = open(config_file, "r")
 	
+	for line in op_config:
+		if line.startswith("blast"):
+			line = line.split("\t")
+			db = line[1]
+			evalue = line[2]
 	return(db, evalue)
