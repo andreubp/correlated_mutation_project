e86b7d3f219edf671fab9cb614bcb5cb09663e7c
mars13 <marina.reixachs@gmail.com>
2016-03-07 14:04:00 +0100

    Blast exec internet

--- modules/blast.py
+++ modules/blast.py
@@ -1,11 +1,17 @@
 from Bio.Blast.Applications import NcbiblastpCommandline as blastp
+from Bio import SeqIO
+from Bio.Blast import NCBIWWW
 from Bio.Blast import NCBIXML
 
 def exec_blast(infile, config_file):
 	db, evalue = parse_blast_conf(config_file)
-	blastp_cline = blastp(query=infile, db=db, evalue = evalue, outfmt=5, out="blast_out.xml")
-	stdout, stderr = blastp_cline()
-	
+	fasta_string = SeqIO.read(infile, format="fasta")
+	result_handle = NCBIWWW.qblast("blastp", "nr", fasta_string.seq)
+	save_file = open("blast_out.xml", "w")
+	save_file.write(result_handle.read())
+	save_file.close()
+	result_handle.close()
+
 def parse_blast_conf(config_file):
 	op_config = open(config_file, "r")
 	
