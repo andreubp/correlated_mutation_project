e0d6f3b90021b063dbaa0da486a4b2b62574f8e1
mars13 <marina.reixachs@gmail.com>
2016-03-17 12:17:27 +0100

    check dependencies

--- main.py
+++ main.py
@@ -73,63 +73,105 @@ parser.add_argument('-hm', '--heatmap',
 
 args = parser.parse_args()
 
-if args.infile2:
-	root = parse_config(args.params, "root")
-	prefix_output = root.rstrip() + args.outfile+"_1"
-	prefix_output_2 = root.rstrip() +args.outfile+"_2"
-	prefix = args.outfile+"_1"
-	prefix_2 =args.outfile+"_2"
-	sys.stderr.write("You have TWO protein sequence into the input and they are saved in all this program with these next prefixes:\n\t%s\n\t%s\n" %(prefix, prefix_2))
-
-	sys.stderr.write("Executing Blast for the %s...\n" %(prefix))
-	file1= exec_blast(args.infile1, args.params, prefix_output)
-	sys.stderr.write("\tBlast finished correctly.\n")
-	sys.stderr.write("Executing Blast for the %s...\n" %(prefix_2))
-	file2= exec_blast(args.infile2, args.params, prefix_output_2)
-	sys.stderr.write("\tBlast finished correctly.\n")
-
-	multifasta1, multifasta2 = get_sequences(args.infile1, file1, args.outfile, args.params, blast_xml_2 = file2, input2 = args.infile2)
-	sys.stderr.write("Running ClustalW for the %s...\n" %(prefix))
-	align = clustalW(multifasta1, args.params, prefix_output+".aln")
-	sys.stderr.write("\tClustalW finished correctly.\n")
-	sys.stderr.write("Running ClustalW for the %s...\n" %(prefix_2))
-	align2 = clustalW(multifasta2, args.params, prefix_output_2+".aln")
-	sys.stderr.write("\tClustalW finished correctly.\n")
-	transposed = read_clustalw(prefix_output+".aln")
-	transposed_2 = read_clustalw(prefix_output_2+".aln")
-	sys.stderr.write("Generating Mutual Information table... You could see it in a few seconds in the next file: %s\n" %(prefix_output+"_mi.tsv"))
-	mi = mutual_information(transposed= transposed, transposed_2 = transposed_2)
-	write_mi_output(mi, prefix_output + "_mi.tsv")
-	sys.stderr.write("Plotting results...\n")
-	plot_heatmap(mi, prefix_output+".png")
-
-else:
-	root = parse_config(args.params, "root")
-	prefix_output = root.rstrip() + args.outfile
-	prefix = args.outfile
-
-	sys.stderr.write("You have only ONE protein sequence into the input with this next prefix:\n\t%s\n" %(prefix))
-
-	sys.stderr.write("Executing Blast...\n")
-	file1= exec_blast(args.infile1, args.params, prefix_output)
-	sys.stderr.write("Blast finished correctly.\n")
-	multifasta1 = get_sequences(args.infile1, file1, prefix_output,args.params)
-
-	sys.stderr.write("Running ClustalW...\n")
-	clustalW(prefix_output+".mfa", args.params, prefix_output+".aln")
-	sys.stderr.write("ClustalW finished correctly.\n")
-	module= read_clustalw(prefix_output+".aln")
-	sys.stderr.write("Generating Mutual Information table. You could see it in a few seconds in the next file: %s\n" %(prefix_output+"_mi.tsv"))
-	mi = mutual_information(module)
-	write_mi_output(mi, prefix_output + "_mi.tsv")
-	sys.stderr.write("Plotting results...\n")
+def checkDependencies():
+	"""
+	Checks the import of the necessary python modules
+	"""
+	try: 
+		import Bio
+		del Bio
+	except ImportError:
+		raise ImportError("ERROR: Unable to import Biopython")
+    
+	try: 
+		import numpy
+		del numpy
+	except ImportError:
+		raise ImportError("ERROR: Unable to import Numpy")
+    
 	if args.heatmap == "plotly":
-		plotly_heatmap(mi, prefix, args.params)
+		try: 
+			import plotly
+			del plotly
+		except ImportError:
+			raise ImportError("ERROR: Unable to import plotly")
+
 	elif args.heatmap == "png":
-		plot_heatmap(mi,prefix_output+'.png')
+		try: 
+			import matplotlib
+			del matplotlib
+		except ImportError:
+			raise ImportError("ERROR: Unable to import Matplotlib")
+	else: 
+		sys.stderr.write("ERROR: The format specified for the heatmap is no available. Try 'png' or 'plotly'\n")
+		exit(1)
+
+
+def runCoevolution():
+	if args.infile2:
+		root = parse_config(args.params, "root")
+		prefix_output = root.rstrip() + args.outfile+"_1"
+		prefix_output_2 = root.rstrip() +args.outfile+"_2"
+		prefix = args.outfile+"_1"
+		prefix_2 =args.outfile+"_2"
+		sys.stderr.write("You have TWO protein sequence into the input and they are saved in all this program with these next prefixes:\n\t%s\n\t%s\n" %(prefix, prefix_2))
+
+		sys.stderr.write("Executing Blast for the %s...\n" %(prefix))
+		file1= exec_blast(args.infile1, args.params, prefix_output)
+		sys.stderr.write("\tBlast finished correctly.\n")
+		sys.stderr.write("Executing Blast for the %s...\n" %(prefix_2))
+		file2= exec_blast(args.infile2, args.params, prefix_output_2)
+		sys.stderr.write("\tBlast finished correctly.\n")
+
+		multifasta1, multifasta2 = get_sequences(args.infile1, file1, args.outfile, args.params, blast_xml_2 = file2, input2 = args.infile2)
+		sys.stderr.write("Running ClustalW for the %s...\n" %(prefix))
+		align = clustalW(multifasta1, args.params, prefix_output+".aln")
+		sys.stderr.write("\tClustalW finished correctly.\n")
+		sys.stderr.write("Running ClustalW for the %s...\n" %(prefix_2))
+		align2 = clustalW(multifasta2, args.params, prefix_output_2+".aln")
+		sys.stderr.write("\tClustalW finished correctly.\n")
+		transposed = read_clustalw(prefix_output+".aln")
+		transposed_2 = read_clustalw(prefix_output_2+".aln")
+		sys.stderr.write("Generating Mutual Information table... You could see it in a few seconds in the next file: %s\n" %(prefix_output+"_mi.tsv"))
+		mi = mutual_information(transposed= transposed, transposed_2 = transposed_2)
+		write_mi_output(mi, prefix_output + "_mi.tsv")
+		sys.stderr.write("Plotting results...\n")
+		
+		if args.heatmap == "plotly":
+			plotly_heatmap(mi, prefix, args.params)
+		elif args.heatmap == "png":
+			plot_heatmap(mi,prefix_output+'.png')
+		sys.stderr.write("All the files generated in this program are saved on:\n%s\n" %(prefix_output))
 	else:
-		sys.stderr.write("This format output is not defined in this program. Try png or plotly")
+		root = parse_config(args.params, "root")
+		prefix_output = root.rstrip() + args.outfile
+		prefix = args.outfile
 
-	sys.stderr.write("All the files generated in this program are saved on:\n%s\n" %(prefix_output))
+		sys.stderr.write("You have only ONE protein sequence into the input with this next prefix:\n\t%s\n" %(prefix))
 
-sys.stderr.write("The program is done. See you!\n")
+		sys.stderr.write("Executing Blast...\n")
+		file1= exec_blast(args.infile1, args.params, prefix_output)
+		sys.stderr.write("Blast finished correctly.\n")
+		multifasta1 = get_sequences(args.infile1, file1, prefix_output,args.params)
+
+		sys.stderr.write("Running ClustalW...\n")
+		clustalW(prefix_output+".mfa", args.params, prefix_output+".aln")
+		sys.stderr.write("ClustalW finished correctly.\n")
+		module= read_clustalw(prefix_output+".aln")
+		sys.stderr.write("Generating Mutual Information table. You could see it in a few seconds in the next file: %s\n" %(prefix_output+"_mi.tsv"))
+		mi = mutual_information(module)
+		write_mi_output(mi, prefix_output + "_mi.tsv")
+		sys.stderr.write("Plotting results...\n")
+		
+		if args.heatmap == "plotly":
+			plotly_heatmap(mi, prefix, args.params)
+		elif args.heatmap == "png":
+			plot_heatmap(mi,prefix_output+'.png')
+		sys.stderr.write("All the files generated in this program are saved on:\n%s\n" %(prefix_output))
+
+	
+if __name__ == "__main__":
+	checkDependencies()
+	sys.stderr.write("Dependencies OK\n")
+	runCoevolution()
+	sys.stderr.write("The program is done. See you!\n")
